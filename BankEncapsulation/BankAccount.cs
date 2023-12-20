9fbd0859569c67d6d3b6f606ce9e5521d429b64d
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-19 19:23:38 -0500

    encapsulation exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0.0;
+
+        public void Deposit(double money)
+        {
+            _balance += money;
+        }
+
+        public double GetBalance()
+        {
+            Console.WriteLine($"Your balance is {_balance}.");
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -2,9 +2,40 @@
 {
     public class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+
+            double input;
+                                  
+
+            Console.WriteLine("Welcome to Bank of Kim.  Press 1 for Depost.  Press 2 for Withdrawl.");
+            int userChoice = int.Parse(Console.ReadLine());
+
+            if (userChoice == 2)
+            {
+                Console.WriteLine("Withdrawls are not accepted at this time, there is no money in the account.");
+
+            }else if (userChoice == 1)
+            {
+                Console.WriteLine("Enter amount to deposit");
+                input = double.Parse(Console.ReadLine());
+                account.Deposit(input);
+                
+                Console.WriteLine($"Deposit accepted, your new balance is {account.GetBalance()}");
+
+
+            }else
+            {
+                Console.WriteLine("Invalid Entry, Please Try Again.");
+            }
+
+
+
+            
+            
+           
         }
     }
 }
